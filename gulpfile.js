9ccda06a4b3ef2422b47f508b1619f785c59ae0e
Ronald Paredes <rparedes@ww-cad-aprout.cad.ww.com>
2017-12-04 14:12:17 -0500

    added imagemin package to optimize image files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
     browserSync = require('browser-sync').create();
     
 gulp.task('watch', () => {
@@ -8,7 +9,12 @@ gulp.task('watch', () => {
       baseDir: './'
     }
   });
-  
-  
-  
+});
+
+gulp.task('OptimizeImg', () => {
+  return gulp.src('./img/railroad/**/*')
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest('./img/OptImg/'));
 });
